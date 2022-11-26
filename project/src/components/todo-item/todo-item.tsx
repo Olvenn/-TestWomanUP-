62479af89bb0835f7907f88997459663cfcbdd36
Olvenn <olga-vencel@yandex.ru>
2022-11-26 17:27:54 +0300

    Added the allocation of an overdue todo

--- project/src/components/todo-item/todo-item.tsx
+++ project/src/components/todo-item/todo-item.tsx
@@ -1,5 +1,5 @@
 import { Todo } from '../../types/types';
-import { collection, addDoc, doc, deleteDoc } from "firebase/firestore";
+import { doc, deleteDoc } from "firebase/firestore";
 import { db } from '../../db/index';
 import dayjs from 'dayjs';
 
@@ -10,13 +10,16 @@ type TodoProps = {
 
 export function TodoItem({ todo, onSave }: TodoProps): JSX.Element {
   const finishedAt = dayjs(todo.finishedAt).format('DD MMM YYYY');
+  const isOverdue = dayjs(todo.finishedAt).diff(dayjs((new Date()))) < 0;
+
   const handleDeleteClick = async (id: string) => {
     await deleteDoc(doc(db, "todoes", id));
     onSave()
   };
 
   return (
-    <div className="container-item modal__content">
+    <div className={`${isOverdue ? 'container-item modal__content overdue' : 'container-item modal__content'}`}
+      data-title="The todo is overdue.">
       <button
         onClick={() => handleDeleteClick(todo.id)}
         className="modal__close-btn button-cross"
